d7e4a7aef5878764b90f86f61aa4769065d5c38f
DiiegoGar <64530932+DiiegoGar@users.noreply.github.com>
2021-04-28 20:31:41 +0200

    Add files via upload

--- CasillaDesplazamiento.java
+++ CasillaDesplazamiento.java
@@ -1,21 +1,22 @@
-package TrabajoOca;
+package proyecto;
 
-public class CasillaDesplazamiento extends Casilla {
 
-	
-	//Constructora
-	public CasillaDesplazamiento(int pNumCasillas) {
-		super(pNumCasillas);
-	
-	}
-	//Otros Metodos	
-	public void realizarAccion(Jugador pJugador) {
-		int casilla=this.getNumCasilla();
-		if (casilla==3) {
-			int n=5;
-			ListaCasillas.getListaCasillas().mover(n, pJugador);
-			ListaJugadores.getListaJugadores().volverATirar();
+	public class CasillaDesplazamiento extends Casilla {
+
+		
+		//Constructora
+		public CasillaDesplazamiento(int pNumCasillas) {
+			super(pNumCasillas);
+		
 		}
+		//Otros Metodos	
+		public void realizarAccion(Jugador pJugador) {
+			int casilla=this.getNumCasilla();
+			if (casilla==3) {
+				int n=5;
+				ListaCasillas.getListaCasillas().mover(n, pJugador);
+				ListaJugadores.getListaJugadores().volverATirar();
+			}
+		}
+		
 	}
-	
-}
--- Normal.java
+++ Normal.java
@@ -1,11 +1,5 @@
-package TrabajoOca;
+package proyecto;
 
-public class Normal extends Casilla {
-	public Normal(int pNumCasillas) {
-		super(pNumCasillas);
-	}
-	//Otros Metodos	
-	public void realizarAccion(Jugador pJugador) {
-		
-	}
+public class Normal {
+
 }
