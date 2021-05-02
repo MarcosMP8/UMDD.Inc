36f8c250d602bed64a7588ca46ee885ecb7041d6
UnaiCastro <83181601+UnaiCastro@users.noreply.github.com>
2021-05-02 13:26:48 +0200

    Add files via upload

--- ListaCasillas.java
+++ ListaCasillas.java
@@ -101,75 +101,7 @@ public class ListaCasillas {
 		return unaCasilla;
 	}
 
-//	public void tablero() {
-	//	for (int pos=0; pos<63; ++pos) {
-		//	if (pos==18 || pos==55 || pos==30 ) {
-			//	Casilla casillaEsperaTurnos= new CasillaEsperaTurnos(pos+1);
-				//this.lista.add(casillaEsperaTurnos);
-			//}
-			//else if (pos==4 || pos==5 || pos==8 || pos==11 || pos==13 || pos==17 || pos==22 || pos==25 || pos==26 || pos==31 || pos==35 || pos==40 || pos==44 || pos==49 || pos==52 || pos==53 || pos==57 || pos==58){
-				//Casilla casillaDesplazamiento= new CasillaDesplazamiento(pos+1);
-				//this.lista.add(casillaDesplazamiento);
-			//}
-			//else {
-				//Casilla casillaNormal= new Normal(pos+1);
-				//this.lista.add(casillaNormal);
-		//	}
-		//}
-	//}
-	
-	public void tablero() {
-		for (int pos=0; pos<63; ++pos) {
-			if (pos==18) {
-				Casilla casillaEsperaTurnos= new CasillaEsperaTurnos(pos+1,2);
-				this.lista.add(casillaEsperaTurnos);
-			}
-			else if (pos==55) {
-				Casilla casillaEsperaTurnos= new CasillaEsperaTurnos(pos+1,3);
-				this.lista.add(casillaEsperaTurnos);
-			}
-			else if (pos==30 ) {
-				Casilla casillaEsperaTurnos= new CasillaEsperaTurnos(pos+1,4);
-				this.lista.add(casillaEsperaTurnos);
-			}
-			else if (pos==8 ||pos==17 ||pos==26 || pos==35 || pos==44 || pos==53 ){
-				Casilla casillaDesplazamiento= new CasillaDesplazamiento(pos+1,5);
-				this.lista.add(casillaDesplazamiento);
-			}
-			else if (pos==4 ||pos==13 ||pos==22 || pos==31 || pos==40 || pos==49 || pos==58 ){
-				Casilla casillaDesplazamiento= new CasillaDesplazamiento(pos+1,4);
-				this.lista.add(casillaDesplazamiento);
-			}
-			else if (pos==5) {
-				Casilla casillaDesplazamiento= new CasillaDesplazamiento(pos+1,6);
-				this.lista.add(casillaDesplazamiento);
-			}
-			else if (pos==11){
-				Casilla casillaDesplazamiento= new CasillaDesplazamiento(pos+1,-6);
-				this.lista.add(casillaDesplazamiento);
-			}
-			else if(pos==25) {
-				Casilla casillaDesplazamiento= new CasillaDesplazamiento(pos+1,25);
-				this.lista.add(casillaDesplazamiento);
-			}
-			else if(pos==52) {
-				Casilla casillaDesplazamiento= new CasillaDesplazamiento(pos+1,-25);
-				this.lista.add(casillaDesplazamiento);
-			}
-			else if(pos==41) {
-				Casilla casillaDesplazamientoNoTirar= new CasillaDesplazamiento(pos+1,-12);
-				this.lista.add(casillaDesplazamientoNoTirar);
-			}
-			else if (pos==57) {
-				Casilla casillaDesplazamientoNoTirar= new CasillaDesplazamiento(pos+1,-57);
-				this.lista.add(casillaDesplazamientoNoTirar);
-			}
-			else {
-				Casilla casillaNormal= new Normal(pos+1);
-				this.lista.add(casillaNormal);
-			}
-		}
-	}
+
 	
 	public int longitudTablero(){
 		return this.lista.size();
