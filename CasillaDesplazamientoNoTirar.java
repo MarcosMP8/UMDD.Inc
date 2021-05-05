de26c80dc28936e8ccceffcf003d5ddd0458b24f
UnaiCastro <83181601+UnaiCastro@users.noreply.github.com>
2021-05-05 13:20:42 +0200

    Add files via upload

--- CasillaDesplazamientoNoTirar.java
+++ CasillaDesplazamientoNoTirar.java
@@ -10,9 +10,9 @@ public class CasillaDesplazamientoNoTirar extends Casilla {
 	public void realizarAccion(Jugador pJugador) {
 		ListaCasillas.getListaCasillas().mover(this.desplazamiento, pJugador);
 		if (this.desplazamiento==-12) {
-			System.out.println("Lo siento, tendras que ir para atras");
+			System.out.println("Has caido en el laberinto, lo siento, tendras que ir para atras");
 		}else {
-			System.out.println("Lo siento, tendras que empezar desde el principio");
+			System.out.println("Has caido en la muerte, lo siento, tendras que empezar desde el principio");
 		}
 	}
 }
--- CasillaEsperaTurnos.java
+++ CasillaEsperaTurnos.java
@@ -14,11 +14,11 @@ public class CasillaEsperaTurnos extends Casilla{
 	public void realizarAccion(Jugador pJugador) {
 		pJugador.setTurnosParados(this.esperaTurnos);
 		if (this.esperaTurnos==2) {
-			System.out.println("Has caido en la Posada :(");
+			System.out.println("Has caido en la Posada, tendras que esperar 2 turnos :(");
 		}else if (this.esperaTurnos==3) {
-			System.out.println("Has caido en la Prision :(");
+			System.out.println("Has caido en la Prision, tendras que esperar 3 turnos :(");
 		}else {
-			System.out.println("Has caido en el Pozo :(");
+			System.out.println("Has caido en el Pozo, tendras que esperar 4 turnos :(");
 		}
 	}		
 }
